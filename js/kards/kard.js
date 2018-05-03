75b1255c27917c7a2a1381948b873ad57213e9b4
Arcgis <arcgis@main.cityoftulsa.org>
2018-05-03 09:20:39 -0500

    minor update I apparently forgot to do

---
+++ js/kards/kard.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+define(
+    [
+        "dojo/_base/declare",
+        "dojo/_base/lang"
+    ],
+    function( declare, lang ){
+        declare( 
+            null,
+            {
+                kard: yes,   // the first property of a kard
+                
+                constructor: function( options ){
+                
+                }
+            }
+        );
+    }
+)
